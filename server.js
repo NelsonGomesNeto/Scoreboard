096696026af237bb529c0f2f6af0d376cdd7947d
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-09-29 23:58:31 -0300

    ...

--- server.js
+++ server.js
@@ -106,40 +106,40 @@ async function updateCompetitionsSubmissions() {
 }
 
 function loadDatabase() {
-  pgdb.query('CREATE TABLE IF NOT EXISTS db(key INTERGER PRIMARY KEY, data JSONB)', (err, res) => {
+  pgdb.query('CREATE TABLE IF NOT EXISTS db(key INTEGER PRIMARY KEY, data JSONB)', (err, res) => {
     if (err) {
       console.log(err);
     }
     pgdb.end();
   });
 
-  // pgdb.query('SELECT * FROM db;', (err, res) => {
-  //   if (err) throw err;
-  //   if (res.rows.length == 0) {
-  //     pgdb.query('INSERT INTO db(key, data) values($1, $2)', [1, '{}'], (err, res) => {
-  //       if (err) {
-  //         console.log(err);
-  //       }
-  //       pgdb.end();
-  //     });
-  //     db = {};
-  //   } else {
-  //     db = res.rows[0];
-  //   }
-  //   pgdb.end();
-  // });
+  pgdb.query('SELECT * FROM db;', (err, res) => {
+    if (err) throw err;
+    if (res.rows.length == 0) {
+      pgdb.query('INSERT INTO db(key, data) values($1, $2)', [1, '{}'], (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+        pgdb.end();
+      });
+      db = {};
+    } else {
+      db = res.rows[0];
+    }
+    pgdb.end();
+  });
 
   // db = fs.readFileSync(dbPath, 'utf8');
   // db = JSON.parse(db);
 }
 
 function saveDatabase() {
-  // pgdb.query('UPDATE db set data = ' + JSON.stringify(db) + ' WHERE key = 1', (err, res) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   pgdb.end();
-  // });
+  pgdb.query('UPDATE db set data = ' + JSON.stringify(db) + ' WHERE key = 1', (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    pgdb.end();
+  });
   // fs.writeFileSync(dbPath, JSON.stringify(db), 'utf8');
 }
 
